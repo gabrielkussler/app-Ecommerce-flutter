f97beb602aa4f75262371861e60c009e114fc687
gabriel <gkgabrielkussler@gmail.com>
2021-05-18 14:23:49 -0300

    tela episodio 4

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,7 +1,18 @@
+import 'dart:html';
+
+import 'package:ecommerceapp/components/product_card.dart';
 import 'package:ecommerceapp/constants.dart';
+import 'package:ecommerceapp/models/Product.dart';
 import 'package:ecommerceapp/size_config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'categories.dart';
+import 'discount_banner.dart';
+import 'home_header.dart';
+import 'popular_products.dart';
+import 'section_title.dart';
+import 'special_offers.dart';
 
 class Body extends StatelessWidget {
   const Body({Key key}) : super(key: key);
@@ -10,63 +21,23 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: getProportionateScreenWidth(20),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: SizeConfig.screenWidth * 0.6,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: kSecondaryColor.withOpacity(0.1),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: TextField(
-                  onChanged: (value) {
-                    // search value
-                  },
-                  decoration: InputDecoration(
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    hintText: "Search Product",
-                    prefixIcon: Icon(Icons.search),
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: getProportionateScreenWidth(20),
-                      vertical: getProportionateScreenWidth(9),
-                    ),
-                  ),
-                ),
-              ),
-              Stack(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(getProportionateScreenWidth(12)),
-                    height: getProportionateScreenWidth(46),
-                    width: getProportionateScreenWidth(46),
-                    decoration: BoxDecoration(
-                      color: kSecondaryColor.withOpacity(0.1),
-                      shape: BoxShape.circle,
-                    ),
-                    child: SvgPicture.asset("assets/icons/Bell.svg"),
-                  ),
-                  Container(
-                    height: getProportionateScreenWidth(16),
-                    width: getProportionateScreenWidth(16),
-                    decoration: BoxDecoration(
-                        color: Color(0xFFFF4848),
-                        shape: BoxShape.circle,
-                        border: Border.all(width: 1.5, color: Colors.white)
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+        child: Column(
+          children: [
+            SizedBox(height: getProportionateScreenWidth(20)),
+            HomeHeader(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            DiscountBanner(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            Categories(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            SpecialOffers(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            PopularProducts(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+          ],
         ),
       ),
     );
   }
 }
+
